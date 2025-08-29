e488625e04887d86f4d49d13bffa541c08b1f45e
Jose Meza <jcdlega@gmail.com>
2025-08-29 11:51:45 -0600

    BUILD: Pilot astronaut model added and exported

---
+++ server/src/models/pilot-astronaut.model.js
@@ -0,0 +1,36 @@
+import mongoose from 'mongoose';
+
+const PilotAstronautScheme = new mongoose.Schema({
+    PilotCallSign:{
+        type: String,
+        required: true,
+        trim: true,
+    },
+    DigitalComunication:{
+        type: String,
+        required: true,
+        trim: true,
+    },
+    Password:{
+        type: String,
+        required: true,
+        trim: true
+    },
+    MissionDesignation:{
+        type: String,
+        trim: true,
+    },
+    PilotID:{
+        type: Number,
+        trim: true,
+    },
+    SpaceAgency:{
+        type: String,
+        trim: true,
+    }
+},{
+    timestamps: true
+}
+);
+
+export default mongoose.model('PilotAstronaut', PilotAstronautScheme);
