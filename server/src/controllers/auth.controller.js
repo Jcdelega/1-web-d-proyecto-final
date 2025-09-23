93b28def3af4776b15ea62fa267a4a2fa28cd318
Jose Meza <jcdlega@gmail.com>
2025-09-22 22:27:13 -0600

    FEAT: Auth added with the routes of register, login and logout

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -1,7 +1,112 @@
-export const register = (req, res)=>{
-    res.send(`Registering...`)
-};
+import pilotAstronautModel from '../models/pilot-astronaut.model.js'
+import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
 
-export const login = (req, res)=>{
-    res.send(`Starting session...`)
-};
+export const register = async (req, res) => {
+    const { PilotCallSign, DigitalCommunication, Password, MissionDesignation, PilotID, SpaceAgency } = req.body
+
+    try {
+
+        const passwordHash = await bcrypt.hash(Password, 10)
+
+        const newPilotAstronaut = new pilotAstronautModel({
+            PilotCallSign,
+            DigitalCommunication,
+            Password: passwordHash,
+            MissionDesignation,
+            PilotID,
+            SpaceAgency
+        })
+
+        const pilotAstronautSaved = await newPilotAstronaut.save();
+        
+        jwt.sign(
+          {
+            id: pilotAstronautSaved._id,
+          },
+          process.env.SECRET_KEY,
+          {
+            expiresIn: '1d',
+          },
+          (err, token) => {
+            if (err) console.log(err);
+            res.cookie('token', token);
+            res.json({
+                id: pilotAstronautSaved._id,
+                PilotCallSign: pilotAstronautSaved.PilotCallSign,
+                DigitalCommunication: pilotAstronautSaved.DigitalCommunication,
+                MissionDesignation: pilotAstronautSaved.MissionDesignation,
+                PilotID: pilotAstronautSaved.PilotID,
+                SpaceAgency: pilotAstronautSaved.SpaceAgency,
+                createdAt: pilotAstronautSaved.createdAt,
+                updatedAt: pilotAstronautSaved.updatedAt,
+            })
+          }
+        );
+        
+
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+}
+
+export const login = async (req, res) => {
+
+  const { DigitalCommunication, Password } = req.body
+
+  try {
+
+    const userFound = await pilotAstronautModel.findOne({ DigitalCommunication })
+    if (!userFound) return res.status(400).json({ message: "User not found" })
+
+    const isMatch = await bcrypt.compare(Password, userFound.Password)
+    if (!isMatch) return res.status(400).json({ message: "Unable to validate, check your credentials" })
+
+    jwt.sign({
+      id: userFound._id
+    },
+      process.env.SECRET_KEY,
+      {
+        expiresIn: "1d"
+      },
+      (err, token) => {
+        if (err) console.log(err)
+        res.cookie('token', token)
+
+        res.json({
+          id: userFound._id,
+          PilotCallSign: userFound.PilotCallSign,
+          DigitalCommunication: userFound.DigitalCommunication,
+          createdAt: userFound.createdAt,
+          updatedAt: userFound.updatedAt,
+        })
+      }
+    )
+
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+  }
+
+}
+
+export const logout = (req, res) => {
+  res.cookie('token', '', {
+    expires: new Date(0)
+  })
+  return res.sendStatus(200)
+}
+
+/* export const profile = async (req, res) => {
+
+  const userFound = await User.findById(req.user.id)
+
+  if (!userFound) return res.status(400).json({ message: "Usuario no encontrado" })
+
+  return res.json({
+    id: userFound._id,
+    PilotCallSign: userFound.PilotCallSign,
+    DigitalCommunication: userFound.DigitalCommunication,
+    createdAt: userFound.createdAt,
+    updatedAt: userFound.updatedAt,
+  })
+} */
