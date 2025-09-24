720f5d2d71bbfe3486fcd96ee0ab2aae357f206e
Jose Meza <jcdlega@gmail.com>
2025-09-24 12:30:55 -0600

    CI: continuous integrations about Routes Authentication and session

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,17 +3,17 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { ConsoleHome } from "./pages/ConsoleHome"
 import { Registration } from "./pages/Registration"
 import { Login } from "./pages/Login"
-import { Session } from "./pages/Session"
+//import { Session } from "./pages/Session"
 //import { PrivateRoute } from "./PrivateRoute"
-//import { AuthProvider } from "./context/AuthContext"
+import { AuthProvider } from "./context/AuthContext"
 //import { Navbar } from "./components/NavBar"
 
 
 export const App = () => {
   return (
-    //<AuthProvider>
+    <AuthProvider>
       <BrowserRouter>
-        <Navbar/>
+        {/* <Navbar/> */}
         <Routes>
           <Route path="/" element={<ConsoleHome />} />
           <Route path="/register" element={<Registration />} />
@@ -32,6 +32,6 @@ export const App = () => {
 
         </Routes>
       </BrowserRouter>
-    //</AuthProvider>
+    </AuthProvider>
   )
 }
---
+++ client/src/context/AuthContext.jsx
@@ -0,0 +1,38 @@
+import { createContext, useContext, useEffect, useState } from 'react';
+import { logoutRequest, sessionRequest } from '../api/authorization.js';
+
+const AuthContext = createContext();
+
+export const useAuth = () => useContext(AuthContext);
+
+export const AuthProvider = ({ children }) => {
+
+  const [pilotAstronaut, setPilotAstronaut] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const checkSession = async () => {
+      try {
+        const res = await sessionRequest();
+        setPilotAstronaut(res.data);
+      } catch (error) {
+        setPilotAstronaut(null);
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    }
+    checkSession();
+  }, []);
+
+  const logout = async () => {
+    await logoutRequest();
+    setPilotAstronaut(null);
+  };
+
+  return (
+    <AuthContext.Provider value={{ pilotAstronaut, loading, logout, setPilotAstronaut }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
